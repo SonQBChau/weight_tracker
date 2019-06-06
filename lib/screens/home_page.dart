c22f33f69e356bfa47f04ec9635fe2176508ff89
Son Chau <soncqb@gmail.com>
2019-06-06 10:04:59 -0500

    add list builder

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,6 +1,19 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<String> myList = [];
+
+  void _handleAddButton() {
+    setState(() {
+      myList.add('new string');
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -8,9 +21,21 @@ class HomePage extends StatelessWidget {
         title: Text('Weight Tracker'),
         backgroundColor: Colors.green,
       ),
-      body: Container(
-        
+      body: ListView.builder(
+        itemCount: myList.length,
+          itemBuilder: (BuildContext ctxt, int index) {
+            return  Text(myList[index]);
+          }
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _handleAddButton();
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.green,
       ),
     );
   }
+
+
 }
